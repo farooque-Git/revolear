a19214f06f7ca7e0277e2aa5935b438b41444293
Farooque <farooque.reactjs@gmail.com>
2025-05-03 14:39:39 +0530

    fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import MainSection from "./home/MainSection";
-import SecondMainScreen from "./home/secondmainscreen";
+
 import ThirdMainScreen from "./home/ThirdMainScreen";
 import ForthMainScreeen from "./home/ForthMainScreeen";
 import FifthMainScreen from "./home/FifthMainScreen";
+import SecondMainScreen from "./home/SecondMainScreen";
 
 const Home = () => {
   return (
