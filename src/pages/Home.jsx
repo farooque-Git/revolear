cf9c451f60ff24276111d2f05b4e5e02379f214f
Farooque <farooque.reactjs@gmail.com>
2025-05-03 14:44:27 +0530

    updatedd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import HeroSection from "./../components/home/HeroSection";
 import MainSection from "./../components/home/MainSection";
+import SecondMainScreen from "../components/home/SecondMainScreen";
+import ThirdMainScreen from "../components/home/ThirdMainScreen";
+import ForthMainScreeen from "./../components/home/ForthMainScreeen";
+import FifthMainScreen from "../components/home/FifthMainScreen";
 
 const Home = () => {
   return (
     <div>
-      <HeroSection />
       <MainSection />
+      <SecondMainScreen />
+      <ThirdMainScreen />
+      <ForthMainScreeen />
+      <FifthMainScreen />
     </div>
   );
 };
